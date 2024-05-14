7137b3b6bd367820376883e0d17327469f2e673a
BinhGiauTen <binhpro50@gmail.com>
2024-05-15 01:52:54 +0700

    add retry

--- BorrowService/src/main/java/com/example/BorrowService/controllers/BorrowController.java
+++ BorrowService/src/main/java/com/example/BorrowService/controllers/BorrowController.java
@@ -4,7 +4,12 @@ package com.example.BorrowService.controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,20 +20,37 @@ import com.example.BorrowService.dto.BorrowRequest;
 import com.example.BorrowService.service.BorrowService;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+//import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 @RequestMapping("/borrows")
+@EnableRetry
 public class BorrowController {
 	
 	@Autowired
 	private BorrowService borrowService;
 	
+	@Bean
+    public RetryTemplate retryTemplate() {
+        RetryTemplate retryTemplate = new RetryTemplate();
+
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(5000); // Retry after 5 seconds
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+        retryPolicy.setMaxAttempts(5); // Maximum retry attempts
+        retryTemplate.setRetryPolicy(retryPolicy);
+
+        return retryTemplate;
+    }
+	
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String borrowBooks(@RequestBody BorrowRequest borrowRequest) {
-        return borrowService.borrowBooks(borrowRequest);
+    	RetryTemplate retryTemplate = retryTemplate();
+        return retryTemplate.execute(context -> borrowService.borrowBooks(borrowRequest)) ;
     }
     
     public String fallbackMethod(BorrowRequest borrowRequest, RuntimeException runtimeException) {
